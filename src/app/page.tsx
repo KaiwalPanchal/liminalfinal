7932794687f00da8c70d5d136b22a32374e6f2a1
Kaiwal <38000481+KaiwalPanchal@users.noreply.github.com>
2024-11-12 07:45:04 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input";
 import { Analytics } from "@vercel/analytics/react";
 import { db } from "@/lib/firebase";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { AppSidebar } from "@/components/ui/app-sidebar"
+import { AppSidebar } from "@/components/ui/app-sidebar";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -15,14 +15,18 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
+} from "@/components/ui/breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"; // Import Collapsible from shadcn
+} from "@/components/ui/sidebar";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"; // Import Collapsible from shadcn
 import { Editor } from "@/components/editor";
 
 interface Note {
@@ -35,160 +39,169 @@ interface Note {
   is_archived: boolean;
 }
 
-
 export default function Component() {
   // const [notes, setNotes] = useState<any>([]);
   const [activeNote, setActiveNote] = useState<any>({
-    "id": "1",
-    "title": "Meeting Notes",
-    "content": "Discussed project timelines and tasks for the next sprint.",
-    "created_at": "2024-11-12T10:00:00Z",
-    "updated_at": "2024-11-12T12:00:00Z",
-    "tags": ["work", "project", "sprint"],
-    "is_archived": false
+    id: "1",
+    title: "Meeting Notes",
+    content: "Discussed project timelines and tasks for the next sprint.",
+    created_at: "2024-11-12T10:00:00Z",
+    updated_at: "2024-11-12T12:00:00Z",
+    tags: ["work", "project", "sprint"],
+    is_archived: false,
   });
-  const notedata:Note[] =  [
-      {
-        "id": "1",
-        "title": "Meeting Notes",
-        "content": "Discussed project timelines and tasks for the next sprint.",
-        "created_at": "2024-11-12T10:00:00Z",
-        "updated_at": "2024-11-12T12:00:00Z",
-        "tags": ["work", "project", "sprint"],
-        "is_archived": false
-      },
-      {
-        "id": "2",
-        "title": "Shopping List",
-        "content": "Milk, Eggs, Bread, Butter, Apples",
-        "created_at": "2024-11-11T08:00:00Z",
-        "updated_at": "2024-11-11T09:00:00Z",
-        "tags": ["personal", "shopping"],
-        "is_archived": false
-      },
-      {
-        "id": "3",
-        "title": "Grocery Store Ideas",
-        "content": "Find a new recipe to try for dinner.",
-        "created_at": "2024-11-10T15:00:00Z",
-        "updated_at": "2024-11-10T16:00:00Z",
-        "tags": ["personal", "ideas"],
-        "is_archived": true
-      },
-      {
-        "id": "4",
-        "title": "Workout Plan",
-        "content": "Monday: Chest, Tuesday: Back, Wednesday: Legs, etc.",
-        "created_at": "2024-11-09T09:00:00Z",
-        "updated_at": "2024-11-09T10:00:00Z",
-        "tags": ["personal", "fitness"],
-        "is_archived": false
-      },
-      {
-        "id": "5",
-        "title": "Travel Plans",
-        "content": "Flight to Paris booked for January, need to plan itinerary.",
-        "created_at": "2024-11-08T16:00:00Z",
-        "updated_at": "2024-11-08T17:00:00Z",
-        "tags": ["travel", "plans"],
-        "is_archived": false
-      },
-      {
-        "id": "6",
-        "title": "Client Feedback",
-        "content": "The client is happy with the current UI design but wants changes to the dashboard layout.",
-        "created_at": "2024-11-07T14:00:00Z",
-        "updated_at": "2024-11-07T15:00:00Z",
-        "tags": ["work", "feedback"],
-        "is_archived": false
-      },
-      {
-        "id": "7",
-        "title": "Book Recommendations",
-        "content": "The Silent Patient by Alex Michaelides, Educated by Tara Westover.",
-        "created_at": "2024-11-06T11:00:00Z",
-        "updated_at": "2024-11-06T12:00:00Z",
-        "tags": ["personal", "books"],
-        "is_archived": false
-      },
-      {
-        "id": "8",
-        "title": "Weekly Goals",
-        "content": "Finish the client report by Thursday, exercise for 30 minutes every day.",
-        "created_at": "2024-11-05T10:00:00Z",
-        "updated_at": "2024-11-05T11:00:00Z",
-        "tags": ["work", "goals"],
-        "is_archived": false
-      },
-      {
-        "id": "9",
-        "title": "Ideas for Blog Post",
-        "content": "Write about productivity tips for remote work.",
-        "created_at": "2024-11-04T09:00:00Z",
-        "updated_at": "2024-11-04T10:00:00Z",
-        "tags": ["work", "blog"],
-        "is_archived": false
-      },
-      {
-        "id": "10",
-        "title": "Weekend Plans",
-        "content": "Visit the art gallery and meet friends for coffee.",
-        "created_at": "2024-11-03T08:00:00Z",
-        "updated_at": "2024-11-03T09:00:00Z",
-        "tags": ["personal", "weekend"],
-        "is_archived": false
-      },
-      {
-        "id": "11",
-        "title": "Important Dates",
-        "content": "John's birthday on November 15, Meeting with Tom on November 18.",
-        "created_at": "2024-11-02T17:00:00Z",
-        "updated_at": "2024-11-02T18:00:00Z",
-        "tags": ["work", "personal"],
-        "is_archived": false
-      },
-      {
-        "id": "12",
-        "title": "Project Ideas",
-        "content": "Create an app for recipe sharing with social features.",
-        "created_at": "2024-11-01T16:00:00Z",
-        "updated_at": "2024-11-01T17:00:00Z",
-        "tags": ["work", "ideas"],
-        "is_archived": false
-      },
-      {
-        "id": "13",
-        "title": "Budget Plan",
-        "content": "Track monthly expenses, set savings goal of $500 each month.",
-        "created_at": "2024-10-31T14:00:00Z",
-        "updated_at": "2024-10-31T15:00:00Z",
-        "tags": ["personal", "finance"],
-        "is_archived": false
-      },
-      {
-        "id": "14",
-        "title": "Dinner Party Menu",
-        "content": "Appetizers: Cheese board, Main Course: Spaghetti, Dessert: Chocolate Cake.",
-        "created_at": "2024-10-30T12:00:00Z",
-        "updated_at": "2024-10-30T13:00:00Z",
-        "tags": ["personal", "food"],
-        "is_archived": false
-      },
-      {
-        "id": "15",
-        "title": "Tech News",
-        "content": "Apple releases new M3 chip, Tesla announces self-driving cars in major cities.",
-        "created_at": "2024-10-29T11:00:00Z",
-        "updated_at": "2024-10-29T12:00:00Z",
-        "tags": ["work", "tech"],
-        "is_archived": false
-      }
-    ]
-console.log(notedata);
+  const notedata: Note[] = [
+    {
+      id: "1",
+      title: "Meeting Notes",
+      content: "Discussed project timelines and tasks for the next sprint.",
+      created_at: "2024-11-12T10:00:00Z",
+      updated_at: "2024-11-12T12:00:00Z",
+      tags: ["work", "project", "sprint"],
+      is_archived: false,
+    },
+    {
+      id: "2",
+      title: "Shopping List",
+      content: "Milk, Eggs, Bread, Butter, Apples",
+      created_at: "2024-11-11T08:00:00Z",
+      updated_at: "2024-11-11T09:00:00Z",
+      tags: ["personal", "shopping"],
+      is_archived: false,
+    },
+    {
+      id: "3",
+      title: "Grocery Store Ideas",
+      content: "Find a new recipe to try for dinner.",
+      created_at: "2024-11-10T15:00:00Z",
+      updated_at: "2024-11-10T16:00:00Z",
+      tags: ["personal", "ideas"],
+      is_archived: true,
+    },
+    {
+      id: "4",
+      title: "Workout Plan",
+      content: "Monday: Chest, Tuesday: Back, Wednesday: Legs, etc.",
+      created_at: "2024-11-09T09:00:00Z",
+      updated_at: "2024-11-09T10:00:00Z",
+      tags: ["personal", "fitness"],
+      is_archived: false,
+    },
+    {
+      id: "5",
+      title: "Travel Plans",
+      content: "Flight to Paris booked for January, need to plan itinerary.",
+      created_at: "2024-11-08T16:00:00Z",
+      updated_at: "2024-11-08T17:00:00Z",
+      tags: ["travel", "plans"],
+      is_archived: false,
+    },
+    {
+      id: "6",
+      title: "Client Feedback",
+      content:
+        "The client is happy with the current UI design but wants changes to the dashboard layout.",
+      created_at: "2024-11-07T14:00:00Z",
+      updated_at: "2024-11-07T15:00:00Z",
+      tags: ["work", "feedback"],
+      is_archived: false,
+    },
+    {
+      id: "7",
+      title: "Book Recommendations",
+      content:
+        "The Silent Patient by Alex Michaelides, Educated by Tara Westover.",
+      created_at: "2024-11-06T11:00:00Z",
+      updated_at: "2024-11-06T12:00:00Z",
+      tags: ["personal", "books"],
+      is_archived: false,
+    },
+    {
+      id: "8",
+      title: "Weekly Goals",
+      content:
+        "Finish the client report by Thursday, exercise for 30 minutes every day.",
+      created_at: "2024-11-05T10:00:00Z",
+      updated_at: "2024-11-05T11:00:00Z",
+      tags: ["work", "goals"],
+      is_archived: false,
+    },
+    {
+      id: "9",
+      title: "Ideas for Blog Post",
+      content: "Write about productivity tips for remote work.",
+      created_at: "2024-11-04T09:00:00Z",
+      updated_at: "2024-11-04T10:00:00Z",
+      tags: ["work", "blog"],
+      is_archived: false,
+    },
+    {
+      id: "10",
+      title: "Weekend Plans",
+      content: "Visit the art gallery and meet friends for coffee.",
+      created_at: "2024-11-03T08:00:00Z",
+      updated_at: "2024-11-03T09:00:00Z",
+      tags: ["personal", "weekend"],
+      is_archived: false,
+    },
+    {
+      id: "11",
+      title: "Important Dates",
+      content:
+        "John's birthday on November 15, Meeting with Tom on November 18.",
+      created_at: "2024-11-02T17:00:00Z",
+      updated_at: "2024-11-02T18:00:00Z",
+      tags: ["work", "personal"],
+      is_archived: false,
+    },
+    {
+      id: "12",
+      title: "Project Ideas",
+      content: "Create an app for recipe sharing with social features.",
+      created_at: "2024-11-01T16:00:00Z",
+      updated_at: "2024-11-01T17:00:00Z",
+      tags: ["work", "ideas"],
+      is_archived: false,
+    },
+    {
+      id: "13",
+      title: "Budget Plan",
+      content: "Track monthly expenses, set savings goal of $500 each month.",
+      created_at: "2024-10-31T14:00:00Z",
+      updated_at: "2024-10-31T15:00:00Z",
+      tags: ["personal", "finance"],
+      is_archived: false,
+    },
+    {
+      id: "14",
+      title: "Dinner Party Menu",
+      content:
+        "Appetizers: Cheese board, Main Course: Spaghetti, Dessert: Chocolate Cake.",
+      created_at: "2024-10-30T12:00:00Z",
+      updated_at: "2024-10-30T13:00:00Z",
+      tags: ["personal", "food"],
+      is_archived: false,
+    },
+    {
+      id: "15",
+      title: "Tech News",
+      content:
+        "Apple releases new M3 chip, Tesla announces self-driving cars in major cities.",
+      created_at: "2024-10-29T11:00:00Z",
+      updated_at: "2024-10-29T12:00:00Z",
+      tags: ["work", "tech"],
+      is_archived: false,
+    },
+  ];
+  console.log(notedata);
 
   return (
     <SidebarProvider>
-      <AppSidebar notes={notedata} setActiveNote={setActiveNote} activeNote={activeNote} />
+      <AppSidebar
+        notes={notedata}
+        setActiveNote={setActiveNote}
+        activeNote={activeNote}
+      />
 
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
@@ -202,9 +215,7 @@ console.log(notedata);
           </div>
 
           {/* Right Div */}
-          <div className="w-1/3 p-4 bg-zinc-900">
-
-          </div>
+          <div className="w-1/3 p-4 bg-zinc-900"></div>
         </div>
       </SidebarInset>
     </SidebarProvider>
